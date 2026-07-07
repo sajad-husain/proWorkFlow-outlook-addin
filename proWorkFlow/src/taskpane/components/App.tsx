f55067efe61d02b169439c04585f1f239b21f69b
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-07-07 13:32:26 +0500

    created app.tsx file whichi is starter file/ entry point

--- proWorkFlow/src/taskpane/components/App.tsx
+++ proWorkFlow/src/taskpane/components/App.tsx
@@ -1,45 +1,30 @@
 import * as React from "react";
+import { makeStyles, tokens } from "@fluentui/react-components";
 import Header from "./Header";
-import HeroList, { HeroListItem } from "./HeroList";
-import TextInsertion from "./TextInsertion";
-import { makeStyles } from "@fluentui/react-components";
-import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
-import { insertText } from "../taskpane";
+import CreateTaskForm from "./CreateTask/CreateTaskForm";
+import { useEmailContext } from "../hooks/useEmailContext";
 
-interface AppProps {
+export interface AppProps {
   title: string;
 }
 
 const useStyles = makeStyles({
   root: {
     minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: tokens.colorNeutralBackground1,
   },
 });
 
 const App: React.FC<AppProps> = (props: AppProps) => {
   const styles = useStyles();
-  // The list items are static and won't change at runtime,
-  // so this should be an ordinary const, not a part of state.
-  const listItems: HeroListItem[] = [
-    {
-      icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration",
-    },
-    {
-      icon: <LockOpen24Regular />,
-      primaryText: "Unlock features and functionality",
-    },
-    {
-      icon: <DesignIdeas24Regular />,
-      primaryText: "Create and visualize like a pro",
-    },
-  ];
+  const { emailData, loading } = useEmailContext();
 
   return (
     <div className={styles.root}>
-      <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
-      <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
-      <TextInsertion insertText={insertText} />
+      <Header logo="assets/logo-filled.png" title={props.title} />
+      <CreateTaskForm emailData={emailData} loading={loading} />
     </div>
   );
 };
