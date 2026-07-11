cc3e7e9203cf2645e7da0fae3581932d49cf0c4e
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-07-11 10:29:54 +0500

    created toast component for notifications

---
+++ proWorkFlow/src/taskpane/components/Toast.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Snackbar, Alert, AlertColor } from "@mui/material";
+
+interface ToastProps {
+  open: boolean;
+  message: string;
+  severity: AlertColor;
+  onClose: () => void;
+  autoHideDuration?: number;
+}
+
+const Toast: React.FC<ToastProps> = ({
+  open,
+  message,
+  severity,
+  onClose,
+  autoHideDuration = 5000,
+}) => {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={autoHideDuration}
+      onClose={onClose}
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+    >
+      <Alert onClose={onClose} severity={severity} variant="filled" sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default Toast;
